a232c2224d83a093ae94da3a508e97fb5c37b980
ovictorqueiroz <victorbr442@gmail.com>
2026-06-03 20:27:52 -0300

    fix: debbugando

--- src/main/java/com/example/Carta.java
+++ src/main/java/com/example/Carta.java
@@ -4,7 +4,8 @@ public class Carta {
     private int numero;
     private Naipe naipe;
 
-    public Carta(Naipe naipe) {
+    public Carta(int numero, Naipe naipe) {
+        this.numero = numero;
         this.naipe = naipe;
     }
 
--- src/main/java/com/example/Monte.java
+++ src/main/java/com/example/Monte.java
@@ -3,17 +3,29 @@ package com.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Monte {
+
     private List<Carta> cartas = new ArrayList<>();
 
-    public Monte() {}
+    public Monte() {
+        Naipe[] np = Naipe.values();
+        for (int linha = 0; linha <= 5; linha++) {
+            Naipe naipe = np[linha];
+            for (int coluna = 1; coluna < 14; coluna++) {
+                Carta carta = new Carta(coluna, naipe);
+                cartas.add(carta);
 
-    public void embaralhar(){
+            }
+        }
+    }
+
+    public void embaralhar() {
         Collections.shuffle(cartas);
     }
 
-    public Carta virar (){
+    public Carta virar() {
         return cartas.get(0);
     }
 }
